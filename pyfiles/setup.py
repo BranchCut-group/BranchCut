aa14705788a625a39f1fde653d9c4708a5112719
nsh-envs <nsh@envs.au.dk>
2024-11-15 11:02:21 +0100

    Added residue calculator, developing shape draw function.

--- pyfiles/setup.py
+++ pyfiles/setup.py
@@ -29,44 +29,66 @@ def wrap(data):
     # Wrap the phase
     return (data+np.pi)%(2*np.pi) - np.pi
 
-def addresidues(data, location, percentage=False):
-    """
-    This Function add residues to a phase diagram. If line is set to True add a horizontal line with center in the given locations.
-    == Input ==
-    data:       (N x M) ndarray of phase diagram.
-
-    location:   (N x 2) ndarray where N is number of residues. Contains (x, y) location of residues or (x, L) location and length 
-                of residue lines.
-
-    line:       Boolean (default False) determines wether the residue is a line or point. If the residue is a line second row of 
-                location is used as length.
-
-    percentage: Boolean (default False) determines wehter all values should be interpreted as percentage of the input dimensions.
-
-    == Output ==
-    data_out:   (N x M) ndarray of phase diagram with residues.
-    """
+def add_residues(data, location, percent=False):
     data_out = np.copy(data)
+
+    if percent:
+        loc = [int(loc[0] * size[0]), int(loc[1] * size[1])]
 
     for row in location:
         if row[2] == 1:
-            data_out[row[0], row[1]] += np.pi
-
+            data_out[row[0], row[1]] += 1.2*np.pi
+    
         else:
-            data_out[row[0], row[1]] -= np.pi
+            data_out[row[0], row[1]] -= 1.2*np.pi
     
     return data_out
 
+def create_mask(size: list, loc: list = [0.5,0.5], shape: int = 0, percent: bool = False):
+    """
+    Create a mask in the shape of a line, square, triangle, or circle.
+    """
+    if percent:
+        loc = [int(loc[0] * size[0]), int(loc[1] * size[1])]
+    
+    mask = np.zeros(size, dtype=np.uint8)
+    
+    match shape:
+        case 0: # line
+            pos = int(size[1]/2)
+            hl  = int(loc[1]/2)
+            mask[loc[0], pos-hl:pos+hl] = 1
+
+        case 1: # square
+            half_size = min(size) // 4
+            mask[loc[0]-half_size:loc[0]+half_size, loc[1]-half_size:loc[1]+half_size] = 1
+
+        case 2: # triangle
+            for i in range(loc[0],loc[0]+(loc[1])):
+                print(loc[0] - (i - loc[1]),loc[0] + (i - loc[1]))
+                #mask[i, ] = 1
+
+        case 3: # circle
+            rr, cc = np.ogrid[:size[0], :size[1]]
+            circle = (rr - loc[1]) ** 2 + (cc - loc[0]) ** 2 <= (min(size) // 4) ** 2
+            mask[circle] = 1
+
+        case _:
+            raise ValueError("Invalid shape. Choose from 0:line, 1:square, 2:triangle, or 3:circle.")
+
+    return mask
+
 def find_residues(data):
+    """
+    Find residues in a wrapped phase diagram
+    """
     # Can be parallelized
     up      = data[:-1,1:]-data[:-1,:-1]
     right   = data[1:,1:]-data[:-1,1:]
     down    = data[1:,:-1]-data[1:,1:]
     left    = data[:-1,:-1]-data[1:,:-1]
 
-    residue_map = up + right + down + left
-
-    print(residue_map)
+    return np.array((wrap(up) + wrap(right) + wrap(down) + wrap(left))/(2*np.pi),dtype="int")
 
 if __name__ == "__main__":
     print("This is a Goldstein Branch Cut algorithm module.")
